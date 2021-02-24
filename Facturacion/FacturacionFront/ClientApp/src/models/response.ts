468c02fdef74e0431ecdbfa5fd27682b711262fa
Johan Mendez <jamg_90_@hotmail.com>
2021-02-24 05:05:21 -0500

    update entidad

--- Facturacion/FacturacionFront/ClientApp/src/models/response.ts
+++ Facturacion/FacturacionFront/ClientApp/src/models/response.ts
@@ -1,7 +1,7 @@
 export interface Response {
-  documento: number;
-  nombres: string;
-  primerApellido: string;
-  segundoApellido: string;
-  edad: number;
+  Documento: number;
+  Nombres: string;
+  PrimerApellido: string;
+  SegundoApellido: string;
+  Edad: number;
 }
